f3a687972782adbf2c98494d359aa79cb376d64d
Shane-C-C-Y <b99611040@ntu.edu.tw>
2015-11-20 20:10:04 -0800

    make in/deterleaver more general

--- deinterleaver.m
+++ deinterleaver.m
@@ -3,16 +3,16 @@ function dnhat = deinterleaver( yp, L, N )
 % Input(yp):    row vector, length N*L???
 % Output:       row vector, size the same
 
-yp_matrix = reshape(yp, N*L, 2);
-dnhat = zeros(size(yp_matrix));
+yp_matrix = reshape(yp, N*L, []);
+dnhat_matrix = zeros(size(yp_matrix));
 
 for i = 1:L
     for j = 1:N
-        dnhat(i+ (j-1)*L, :) = yp_matrix((i-1)*N+j, :);
+        dnhat_matrix(i+ (j-1)*L, :) = yp_matrix((i-1)*N+j, :);
     end
 end
 
-dnhat = dnhat(:)';
+dnhat = dnhat_matrix(:)';
 
 end
 
--- interleaver.m
+++ interleaver.m
@@ -1,21 +1,18 @@
 function xp = interleaver( dn, L, N)
 
-% here we have two set of N*L
 % Input(dn):    row vector, length N*L
 % Output:       row vector, size the same
 
-assert(length(dn) == 2*N*L);
-
-dn_matrix = reshape(dn, N*L, 2);
-xp2 = zeros(size(dn_matrix));
+dn_matrix = reshape(dn, N*L, []);
+xp_matrix = zeros(size(dn_matrix));
 
 for i = 1:L
     for j = 1:N
-        xp2((i-1)*N+j, :) = dn_matrix(i+ (j-1)*L, :);
+        xp_matrix((i-1)*N+j, :) = dn_matrix(i+ (j-1)*L, :);
     end
 end
 
-xp = xp2(:)';
+xp = xp_matrix(:)';
 
 end
 
